938f573e1b9ca0c3aafc2f90c15ae684d018b986
Tomo Hung <tomohung@gmail.com>
2017-08-05 01:22:38 +0800

    Add TODO

--- src/index.js
+++ src/index.js
@@ -63,3 +63,49 @@ const todoApp = combineReducers({
 });
 
 const store = createStore(todoApp);
+
+const { Component } = React;
+
+let nextTodoId = 0;
+
+class TodoApp extends Component {
+  render() {
+    return (
+        <div>
+        <input ref={node => {
+          this.input = node;
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          this.input.value = '';
+          this.input.focus();
+        }}>
+        Add Todo
+      </button>
+        <ul>
+        {this.props.todos.map(todo =>
+                              <li key={todo.id}>
+                              {todo.text}
+                              </li>
+                             )}
+      </ul>
+        </div>
+    );
+  };
+}
+
+const render = () => {
+  ReactDOM.render(
+      <TodoApp
+    todos={store.getState().todos}
+      />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
